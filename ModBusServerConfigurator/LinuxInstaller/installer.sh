395cff3805aefc9cf65d5936fe68a748939afffc
Federico Turco <>
2022-06-14 23:40:24 +0200

    Added conf files

--- ModBusServerConfigurator/LinuxInstaller/installer.sh
+++ ModBusServerConfigurator/LinuxInstaller/installer.sh
@@ -1,7 +1,7 @@
 
-PATH_GIT_PROJECT="https://github.com/Fedex1515/ModBusServerConfigurator/settings"
-
 sudo su
+
+PATH_GIT_PROJECT="https://github.com/Fedex1515/ModBusServerConfigurator/settings"
 
 apt-get install -y python3-pip
 apt-get install -y git
@@ -11,6 +11,8 @@ apt-get install -y dnsmasq
 pip3 install pymodbus
 pip3 install pyserial
 
+git clone $PATH_GIT_PROJECT
+
 cp ModBusServerConfigurator/LinuxInstaller/hostapd.conf /etc/hostapd/hostapd.conf
 cp ModBusServerConfigurator/LinuxInstaller/dnsmasq.conf /etc/dnsmasq.conf
 
@@ -18,9 +20,8 @@ cp ModBusServerConfigurator/PythonEngine /home/pi/ModBusServerConfigurator
 
 chown pi /home/pi/ModBusServerConfigurator/config.json
 
-ln -s 
-
-git clone $PATH_GIT_PROJECT
+ln -s /home/pi/ModBusServerConfigurator/modbus.service /etc/systemd/system/modbus.service
+systemctl enable modbus.service
 
 exit
 
